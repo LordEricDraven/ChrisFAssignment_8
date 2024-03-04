c88497cad3ff6826925eb1c53c4e158a9ff2b859
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-03-03 19:31:04 -0500

    Starting Assignment8

---
+++ Assignment_8/src/com/coderscampus/assignment/Assignment8Service.java
@@ -0,0 +1,15 @@
+package com.coderscampus.assignment;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Assignment8Service {
+
+	private final Assignment8 assignment;
+	
+	public Assignment8Service() {
+		assignment = new Assignment8();
+				
+	}
+	
+}
---
+++ Assignment_8/src/com/coderscampus/assignment/Main.java
@@ -0,0 +1,11 @@
+package com.coderscampus.assignment;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Assignment8Service service = new Assignment8Service();
+		
+	}
+
+}
