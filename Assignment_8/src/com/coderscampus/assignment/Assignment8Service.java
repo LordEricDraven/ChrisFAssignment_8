ac8ba92d37a6bcd661e40eb6ea0e37ff35520f78
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-03-05 18:59:22 -0500

    current update to assignment

--- Assignment_8/src/com/coderscampus/assignment/Assignment8Service.java
+++ Assignment_8/src/com/coderscampus/assignment/Assignment8Service.java
@@ -1,15 +1,45 @@
 package com.coderscampus.assignment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Assignment8Service {
 
-	private final Assignment8 assignment;
+	private final Assignment8 assignment = new Assignment8();
+	private final List<Integer> numbersList = new ArrayList<>();
 	
-	public Assignment8Service() {
-		assignment = new Assignment8();
-				
+	private final ExecutorService singleThread = Executors.newSingleThreadExecutor();
+	private final ExecutorService cachedThread = Executors.newCachedThreadPool();
+	
+	public void getAllNumbers() {
+		List<CompletableFuture<Void>> futures = new ArrayList<>();
+		for (int i = 0; i < 1000; i++) {
+			CompletableFuture<Void> future = CompletableFuture.supplyAsync(assignment::getNumbers, cachedThread)
+							 .thenAcceptAsync(numbersList::addAll, singleThread);
+			futures.add(future);
+		}
+		CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+						 .thenRunAsync(this::CountAndPrint)
+						 .join();
 	}
 	
+	private void CountAndPrint() {
+		System.out.println("All numbers fetched. Total: " + numbersList.size());
+		Map<Integer, Long> occurences = CountOccurences(numbersList);
+		occurences.forEach((number, count) -> {
+			System.out.println("Number: " + number + ", Count: " + count);
+		});
+	}
+	
+	private Map<Integer, Long> CountOccurences(List<Integer> numbers){
+		return numbers.stream()
+					  .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		
+	}
 }
--- Assignment_8/src/com/coderscampus/assignment/Main.java
+++ Assignment_8/src/com/coderscampus/assignment/Main.java
@@ -1,11 +1,15 @@
 package com.coderscampus.assignment;
 
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		Assignment8Service service = new Assignment8Service();
 		
+		service.getAllNumbers();
 	}
-
 }
+
+
+
